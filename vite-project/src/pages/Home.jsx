ee4f2744ef1570d9eab3c702b29c184ff5d8e0ee
Yorian Fransz <sydney-cook@outlook.com>
2026-04-19 13:53:33 +0200

    install react-calender werken aan boilerplate

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,8 +1,37 @@
-function Home (){
-    return(
-        <>
-        </>
-    )
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+
+function Home() {
+  const [value, setValue] = useState(new Date());
+
+  return (
+    <>
+      <header>
+        <h1>Novi-Naut in Space</h1>
+      </header>
+
+      <main>
+        <section>
+          <article>
+            <Calendar
+              onChange={setValue}
+              value={value}
+            />
+          </article>
+        </section>
+
+        <section>
+          <button>Next</button>
+          <button>Back</button>
+        </section>
+      </main>
+
+      <footer>
+        <h1>PixelPopping@Productions</h1>
+      </footer>
+    </>
+  );
 }
 
 export default Home;
+
