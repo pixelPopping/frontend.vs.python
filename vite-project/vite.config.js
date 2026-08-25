707278ed21fd280a08825b4271fa627f79ff9e5a
Yorian Fransz <sydney-cook@outlook.com>
2026-08-25 11:56:33 +0200

    Fix Vite production build

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,9 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.js",
+  },
+
+  build: {
+    cssMinify: "esbuild",
   },
 });
